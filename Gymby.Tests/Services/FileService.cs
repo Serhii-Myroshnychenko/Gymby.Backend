b238a3da4a755db5cca6038aae16da078215c6dd
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2023-05-24 16:38:21 +0300

    Edited FileService

--- Gymby.Tests/Services/FileService.cs
+++ Gymby.Tests/Services/FileService.cs
@@ -14,13 +14,20 @@ namespace Gymby.UnitTests.Services
         public FileService()
         {
         }
-        public async Task AddPhotoAsync(string containerName, string userId, string folderName, IFormFile file)
+
+        public async Task AddPhotoAsync(string containerName, string userId, string folderName, IFormFile file, string newFileName)
         {
             
         }
+
         public async Task DeletePhotoAsync(string containerName, string userId, string folderName)
         {
 
+        }
+
+        public async Task DeleteSelectedPhoto(string containerName, string userId, string folderName, string fileName)
+        {
+            
         }
 
         public Task<List<string>> GetListOfPhotos(string containerName, string userId, string folderName)
