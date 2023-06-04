b146cbd1bb760fcb81dbf95c154c4a54f175f8dc
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2023-06-04 16:37:00 +0300

    Added QueryProgramHandler

---
+++ Gymby.Application/Mediatr/Programs/Queries/QueryProgram/QueryProgramQuery.cs
@@ -0,0 +1,12 @@
+﻿using Gymby.Application.ViewModels;
+using MediatR;
+
+namespace Gymby.Application.Mediatr.Programs.Queries.QueryProgram;
+
+public class QueryProgramQuery : IRequest<List<ProgramVm>>
+{
+    public string? Level { get; set; }
+    public string? Type { get; set; }
+    public string? Query { get; set; }
+    public string UserId { get; set; } = null!;
+}
