a7b1428b293248f0cb5828d5f6e7cd6eff96ce0f
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2023-06-05 13:29:49 +0300

    Added friend query

---
+++ Gymby.Application/Mediatr/Friends/Queries/QueryFriends/QueryFriendHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Gymby.Application.Mediatr.Friends.Queries.GetMyFriendsList;
+using Gymby.Application.ViewModels;
+using MediatR;
+
+namespace Gymby.Application.Mediatr.Friends.Queries.QueryFriends;
+
+public class QueryFriendHandler 
+    : IRequestHandler<QueryFriendQuery, List<ProfileVm>>
+{
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    public QueryFriendHandler(IMapper mapper, IMediator mediator) =>
+        (_mapper, _mediator) = (mapper, mediator);
+
+    public async Task<List<ProfileVm>> Handle(QueryFriendQuery request, CancellationToken cancellationToken)
+    {
+        var friends = await _mediator.Send(new GetMyFriendsListQuery(request.Options) { UserId = request.UserId, Options = request.Options},cancellationToken);
+
+        if (request.Type == "trainers")
+        {
+            friends = friends.Where(p => p.IsCoach == true).ToList();
+        }
+
+        if (request.Query != null)
+        {
+            friends = friends.Where(p => p.Username!.Contains(request.Query)).ToList();
+        }
+
+        return _mapper.Map<List<ProfileVm>>(friends);
+    }
+}
---
+++ Gymby.WebApi/Models/ResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Gymby.WebApi.Models;
+
+public class ResponseModel
+{
+    public string Signature { get; set; }
+    public string Data { get; set; }
+}
